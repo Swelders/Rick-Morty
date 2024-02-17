9ca2aeb4ac84aedfdb442165095f587acf9a0b8c
Swelders <grzegorski26@gmail.com>
2024-02-17 13:41:27 +0100

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Jakub Grzegorski on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
